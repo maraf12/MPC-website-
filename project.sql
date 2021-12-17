b611e80c744a490ec8ba88527d0f347ce6102ad0
maraf12 <80293557+maraf12@users.noreply.github.com>
2021-12-17 19:24:04 +0100

    Update project.sql

--- project.sql
+++ project.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Apr 12, 2017 at 12:19 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- exported from phpmyadmin 
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
